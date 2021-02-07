cd39b8aa36ff21a06d94012495616f42903720ce
ogi <yeet.ofed@gmail.com>
2021-02-07 15:32:38 +0100

    small bugfix

--- src/Components/SecondPageComponents/HeaderBox.jsx
+++ src/Components/SecondPageComponents/HeaderBox.jsx
@@ -13,7 +13,7 @@ const HeaderBox = () => {
                     Hire Remotely
                 </div>
                 <div className="">
-                    <button type="button" className="transform hover:scale-105 motion-reduce:transform-none ml-3 px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-800 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    <button type="button" className="transform lg:inline-flex lg:items-center lg:px-4 lg:py-3  hover:scale-105 motion-reduce:transform-none ml-3 px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-800 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                         <Link to="/">Start now!</Link>
                     </button>
                 </div>
