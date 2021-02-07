88ec2aa73ae02e84be66aa5441eec78ebc790e0d
ogi <yeet.ofed@gmail.com>
2021-02-07 15:21:45 +0100

    styling fix

--- src/Components/SecondPageComponents/HeaderBox.jsx
+++ src/Components/SecondPageComponents/HeaderBox.jsx
@@ -5,7 +5,7 @@ import { Route, BrowserRouter as Router, Link } from 'react-router-dom'
 const HeaderBox = () => {
     return (
         <div className="pt-1">
-            <div className="flex lg:flex lg:p-2 lg:items-center border-b lg:justify-between p-0 justify-center">
+            <div className="flex lg:flex lg:p-2 lg:items-center items-center border-b lg:justify-between p-0 justify-center">
                 <div className="">
                     <img src={Logo} />
                 </div>
@@ -13,7 +13,7 @@ const HeaderBox = () => {
                     Hire Remotely
                 </div>
                 <div className="">
-                    <button type="button" className="transform hover:scale-105 motion-reduce:transform-none ml-3 px-4 py-3 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-800 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    <button type="button" className="transform hover:scale-105 motion-reduce:transform-none ml-3 px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-800 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                         <Link to="/">Start now!</Link>
                     </button>
                 </div>
